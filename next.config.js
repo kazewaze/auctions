f200f5d96d14ea27ea450fef673fc5430138d41c
Kaycee Ingram <KayceeWilliamIngram@gmail.com>
2022-11-24 14:05:22 -0800

    fixed prod issue

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
+            : 'https://ronnieingramauctions.vercel.app/api' // production api
     }
 }

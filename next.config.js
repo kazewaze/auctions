eb1e05af34bdc2696360ef8e854675c52ad49472
Kaycee Ingram <KayceeWilliamIngram@gmail.com>
2022-12-01 12:05:58 -0800

    edit error

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
-const securityHeaders = [
-    {
-      key: 'X-Content-Type-Options',
-      value: 'nosniff'
-    },
-    {
-      key: 'X-DNS-Prefetch-Control',
-      value: 'on'
-    },
-    {
-      key: 'X-XSS-Protection',
-      value: '1; mode=block'
-    },
-    {
-      key: 'X-Frame-Options',
-      value: 'SAMEORIGIN'
-    },
-    {
-      key: 'Permissions-Policy',
-      value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
-    },
-    {
-      key: 'Referrer-Policy',
-      value: 'origin-when-cross-origin'
-    }
-  ];
+// const securityHeaders = [
+//     {
+//       key: 'X-Content-Type-Options',
+//       value: 'nosniff'
+//     },
+//     {
+//       key: 'X-DNS-Prefetch-Control',
+//       value: 'on'
+//     },
+//     {
+//       key: 'X-XSS-Protection',
+//       value: '1; mode=block'
+//     },
+//     {
+//       key: 'X-Frame-Options',
+//       value: 'SAMEORIGIN'
+//     },
+//     {
+//       key: 'Permissions-Policy',
+//       value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+//     },
+//     {
+//       key: 'Referrer-Policy',
+//       value: 'origin-when-cross-origin'
+//     }
+//   ];
   
   module.exports = {
     reactStrictMode: true,
@@ -33,20 +33,20 @@ const securityHeaders = [
     publicRuntimeConfig: {
         apiUrl: 'https://ronnieingram.vercel.app/api'
     },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: securityHeaders,
-        },
-      ]
-    },
+    // async headers() {
+    //   return [
+    //     {
+    //       source: '/(.*)',
+    //       headers: securityHeaders,
+    //     },
+    //   ]
+    // },
     async rewrites() {
       return [
         {
           source: '/api/:path*',
           destination: 'https://ronnieingram.vercel.app/:path*',
-          },
+        },
         ]
     },
   };

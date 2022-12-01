8c0a342e3dffd498875db81872403f4bbbc3fe8a
Kaycee Ingram <KayceeWilliamIngram@gmail.com>
2022-11-30 22:47:31 -0800

    auth error fix maybe again

--- next.config.js
+++ next.config.js
@@ -1,11 +1,44 @@
-module.exports = {
+const securityHeaders = [
+    {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff'
+    },
+    {
+      key: 'X-DNS-Prefetch-Control',
+      value: 'on'
+    },
+    {
+      key: 'X-XSS-Protection',
+      value: '1; mode=block'
+    },
+    {
+      key: 'X-Frame-Options',
+      value: 'SAMEORIGIN'
+    },
+    {
+      key: 'Permissions-Policy',
+      value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+    },
+    {
+      key: 'Referrer-Policy',
+      value: 'origin-when-cross-origin'
+    }
+  ];
+  
+  module.exports = {
     reactStrictMode: true,
     serverRuntimeConfig: {
         secret: process.env.SECRET
     },
     publicRuntimeConfig: {
-        apiUrl: process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3000/api' // development api
-            : 'https://ronnieingramauctions.vercel.app/api' // production api
+        apiUrl: 'https://ronnieingramauctions.vercel.app/api'
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: securityHeaders,
+        },
+      ]
     }
-}
+  };
